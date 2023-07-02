95befbc225f1d689765fd9682b6ba65f79860468
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-07-02 18:06:08 +0300

    fix

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import JellyScroll from './screens/JellyScroll.js';
+import Carousel from './screens/Carousel.js';
 
 const App = () => {
   return (
     <View style={{flex: 1}}>
-     <JellyScroll/>
+      <Carousel />
     </View>
   );
 }
