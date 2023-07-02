54c3d915e2800ba02716974a9ee16c5be023d4dc
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-07-02 18:04:11 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import JellyScroll from './screens/JellyScroll.js';
+
+const App = () => {
+  return (
+    <View style={{flex: 1}}>
+     <JellyScroll/>
+    </View>
+  );
+}
+
+export default App;
